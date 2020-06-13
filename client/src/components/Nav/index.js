14421652620d0ffc651a6aa34822d9474fa65d8b
Ian J <ianbennettjohnson@gmail.com>
2020-06-13 13:37:06 -0700

    Updated Nav Component so the logo and title are in place in left corner

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,10 +1,34 @@
 import React from "react";
 
 const Nav = () => {
+  const imgStyle = {
+    width: "65px",
+    height: "65px",
+    marginRight: "10px",
+    marginBottom: "25px",
+    position:"absolute"
+  }
+  const wordStyle = {
+    position:"relative",
+    paddingLeft: "75px",
+    paddingTop:"12px",
+    color: "cornsilk",
+    fontSize: "30px",
+    fontFamily: "Chalkduster, fantasy"
+  }
+ 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/"><img src="../img/quokka-logo.png" alt="quokka-image"/>quokka
+      <a 
+        className="navbar-brand" 
+        href="/"
         
+      >
+        <img src="../img/quokka-logo.png" 
+          alt="quokka-image"
+          style = {imgStyle} 
+        />
+           <p style={wordStyle}>quokka</p>
       </a>
     </nav>
     
