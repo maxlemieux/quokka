48f0541d2d15bf76c350b76d4bda18018bc694a6
Ian J <ianbennettjohnson@gmail.com>
2020-06-13 13:57:47 -0700

    Updated navabr

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,21 +4,26 @@ const Nav = () => {
   const imgStyle = {
     width: "65px",
     height: "65px",
-    marginRight: "10px",
-    marginBottom: "25px",
+    margin: "0px 10px 25px 0px",
     position:"absolute"
   }
   const wordStyle = {
     position:"relative",
-    paddingLeft: "75px",
-    paddingTop:"12px",
+    padding: "12px 0px 0px 75px",
     color: "cornsilk",
     fontSize: "30px",
     fontFamily: "Chalkduster, fantasy"
   }
+  const navStyle ={
+    boxShadow: "0px 5px 10px 5px pink",
+    marginBottom: "20px"
+  }
  
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav 
+    className="navbar navbar-expand-lg navbar-dark bg-primary"
+    style= {navStyle}
+    >
       <a 
         className="navbar-brand" 
         href="/"
