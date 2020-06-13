4c4a99b6f5aef47875e14c9aded01eeed801676b
Ian J <ianbennettjohnson@gmail.com>
2020-06-13 12:49:24 -0700

    Added img folder in public and a quokka logo image

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,10 +3,11 @@ import React from "react";
 const Nav = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
+      <a className="navbar-brand" href="/"><img src="../img/quokka-logo.png" alt="quokka-image"/>quokka
+        
       </a>
     </nav>
+    
   );
 }
 
