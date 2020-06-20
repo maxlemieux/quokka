234427077ccd2a6f77de49bc74ff2c3df01f8d15
Max Lemieux <max.lemieux@gmail.com>
2020-06-20 10:48:03 -0700

    moved book API to plant API

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,11 @@
+const axios = require("axios");
 const router = require("express").Router();
-const bookRoutes = require("./books");
+
 const plantRoutes = require("./plants");
 const trefleRoutes = require("./trefle");
 const phzmapiRoutes = require("./phzmapi");
-const axios = require("axios");
 
 // Book routes
-router.use("/books", bookRoutes);
 router.use("/plants", plantRoutes);
 router.use("/trefle", trefleRoutes);
 router.use("/phzmapi", phzmapiRoutes);
