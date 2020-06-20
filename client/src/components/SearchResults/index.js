7ffe688a74f654aebba769563a24dbd7ac74524f
Max Lemieux <max.lemieux@gmail.com>
2020-06-20 13:43:57 -0700

    favorite button

--- client/src/components/SearchResults/index.js
+++ client/src/components/SearchResults/index.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import { List, ListItem } from "../List";
+import API from "../../utils/API";
 
 export function SearchResults(props, { children }) {
+  const [plants, setPlants] = useState([])
+  function loadPlants() {
+    API.getPlants()
+      .then(res => 
+        setPlants(res.data)
+      )
+      .catch(err => console.log(err));
+  };
+  function savePlant() {
+    // console.log('Saved plant:')
+    // console.log(plant)
+    API.savePlant({
+      scientific_name: 'foobarbaz',
+      trefle_id: 12345,
+    })
+      .then(res => loadPlants())
+      .catch(err => console.log(err));
+  }
   // console.log(props);
   return (
     <div className="list-overflow-container">
-      {props.searchResults.data && props.searchResults.data.map(result => <Result result={result} key={result.id} />)}
+      {props.searchResults.data && props.searchResults.data.map(result => <Result savePlant={savePlant} result={result} key={result.id} />)}
       <ul className="list-group">{children}</ul>
     </div>
   );
@@ -14,7 +34,13 @@ export function SearchResults(props, { children }) {
 export function Result(props, { children }) {
   return (
     <li className="list-group-item">
-      {props.result.scientific_name}
+      <List>
+        <ListItem>
+          Scientific Name: {props.result.scientific_name}
+        </ListItem>
+        {props.result.common_name && <ListItem>`Common Name: ${props.result.common_name}`</ListItem>}
+      </List>
+      <button onClick={props.savePlant}>Save to Favorites</button>
       {children}
     </li>
   );
