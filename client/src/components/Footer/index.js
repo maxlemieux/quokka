08b230569498b082577312378f187f422603e022
Ian J <ianbennettjohnson@gmail.com>
2020-06-26 09:56:37 -0700

    Footer styled to bottom

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 const Footer = () => { 
+  const styleFooter = {
+    position: 'fixed',
+    left: '0',
+    bottom: '0',
+    width: '100%',
+    textAlign: 'center',
+    backgroundColor: '#78C2AD',
+    color: 'cornsilk',
+    boxShadow: "0px 0px 10px 15px pink",
+  }
+  const styleA = {
+    color: 'pink',
+  }
   return (
-    <container>
+    <div style={styleFooter}>
       <p>Quokka (c) 2020 by Andy J Chen, Ian Johnson, Max Lemieux</p>
-      <p>This product includes GeoLite2 data created by MaxMind, available from <a href="https://www.maxmind.com">https://www.maxmind.com</a></p>
-    </container>
+      <p>This product includes GeoLite2 data created by MaxMind, available from <a style ={styleA} href="https://www.maxmind.com">https://www.maxmind.com</a></p>
+    </div>
   );
 }
 
