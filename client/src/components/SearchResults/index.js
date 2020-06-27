d085363911b8d667b50ee1163020546f18f93cf7
Max Lemieux <max.lemieux@gmail.com>
2020-06-27 15:35:26 -0700

    fixed more style warnings

--- client/src/components/SearchResults/index.js
+++ client/src/components/SearchResults/index.js
@@ -1,31 +1,30 @@
-import React, {useState} from "react";
-import "./style.css";
-import { List, ListItem } from "../List";
-import API from "../../utils/API";
+import React, { useState } from 'react';
+import './style.css';
+import { List, ListItem } from '../List';
+import API from '../../utils/API';
 // import { Spinner } from 'react-bootstrap';
 // import ReactSpinner from 'react-bootstrap-spinner';
 
 export function SearchResults(props) {
   function loadPlants() {
     API.getPlants()
-      .then(res => {
-          props.setPlants(res.data)
-        }
-      )
-      .catch(err => console.log(err));
-  };
-  
+      .then((res) => {
+        props.setPlants(res.data);
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div className="list-overflow-container">
-  {props.showSpinner && 
-  <div>
+  {props.showSpinner
+    && <div>
     {/* <ReactSpinner type='border' color='primary' size='5' /> */}
     <p>Loading...</p>
     </div>
   }
       <ul className="list-group">
-        {props.searchResults.data && 
-           props.searchResults.data.map(result => 
+        {props.searchResults.data
+          && props.searchResults.data.map((result) => (
             <Result
               userName={props.userName}
               userIp={props.userIp}
@@ -34,49 +33,42 @@ export function SearchResults(props) {
               result={result}
               key={result.id}
             />
-          )
-        } 
+          ))
+        }
       </ul>
     </div>
-    
   );
 }
 
 export function Result(props) {
-  const [isFavorite, setIsFavorite] = useState(false)
+  const [isFavorite, setIsFavorite] = useState(false);
   function savePlant(plantId) {
-    console.log(`saving plant with id ${plantId}`)
     API.plantDetails(plantId)
-      .then(res => {
+      .then((res) => {
         res.data.user_name = props.userName;
         res.data.ip = props.userIp;
-        res.data.trefle_id = res.data.id
-        console.log('attempting to call API.savePlant with res.data:')
-        console.log(res.data)
+        res.data.trefle_id = res.data.id;
         API.savePlant(res.data)
           .then(res => {
-            console.log('savePlant fired in Result component')
             props.loadPlants();
             props.loadFavorites();
           })
-        })
-      .catch(err => console.log(err))
+      })
+      .catch((err) => console.log(err));
   }
 
   API.getPlant(props.result.id)
-    .then(res => {
-      console.log(`response from getPlant for the id ${props.result.id}`)
-      // console.log(res)
-      if (res.data.exists){
-        setIsFavorite(true)}
-      else {
-        setIsFavorite(false)
+    .then((res) => {
+      if (res.data.exists) {
+        setIsFavorite(true);
+      } else {
+        setIsFavorite(false);
       }
-      console.log(`${props.result.scientific_name} isFavorite Status is ${isFavorite }`)
+      console.log(`${props.result.scientific_name} isFavorite Status is ${isFavorite}`);
       // console.log(res)
     })
-    .catch(err => console.log(err))
-  
+    .catch((err) => console.log(err));
+
   return (
     <li className="list-group-item">
       <List>
@@ -85,11 +77,11 @@ export function Result(props) {
           Scientific Name: {props.result.scientific_name}
         </ListItem>
       </List>
-      {!isFavorite &&
-        <button onClick={() => { savePlant(props.result.id) }}>Save to Favorites</button>
+      {!isFavorite
+        && <button onClick={() => { savePlant(props.result.id); }}>Save to Favorites</button>
       }
-      {isFavorite &&
-        <strong>Favorite!</strong>
+      {isFavorite
+        && <strong>Favorite!</strong>
       }
     </li>
   );
