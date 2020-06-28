be4ec824223d5c77229385909ff4cbf08612b6aa
Max Lemieux <max.lemieux@gmail.com>
2020-06-27 20:36:39 -0700

    activity feed shows pictures

--- client/src/components/ActivityFeed/index.js
+++ client/src/components/ActivityFeed/index.js
@@ -1,3 +1,5 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
@@ -7,8 +9,8 @@ export default function ActivityFeed(props) {
     <div>
       <ul>
         {props.data.map((favorite) => (
-          <li key={favorite.id}>
-            {favorite.images[1] && <img src={favorite.images[1]} alt={favorite.scientific_name} />}
+          <li className='list-overflow-container' key={favorite._id}>
+            {favorite.images[1] && <img width='100%' src={favorite.images[1].url} alt={favorite.scientific_name} />}
             {favorite.user_name} quokked {favorite.common_name
               ? favorite.common_name : favorite.scientific_name}
           </li>
