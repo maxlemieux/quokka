5f343f9d4a4c99267a68265c12ed25b13919585f
Max Lemieux <max.lemieux@gmail.com>
2020-06-28 20:50:24 -0700

    removed logging

--- routes/api/geoip.js
+++ routes/api/geoip.js
@@ -5,7 +5,6 @@ const maxmind = require('maxmind');
 router.route('/:ip')
   .get((req, res) => {
     const { ip } = req.params;
-    console.log(ip);
     maxmind.open('./geoip/GeoLite2-City.mmdb')
       .then((lookup) => {
         res.json(lookup.get(ip));
