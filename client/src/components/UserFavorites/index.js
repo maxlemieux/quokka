6a341cc75ad7ea0e938734531d7a3074122d3b5e
Max Lemieux <max.lemieux@gmail.com>
2020-06-28 21:45:25 -0700

    commented unused code

--- client/src/components/UserFavorites/index.js
+++ client/src/components/UserFavorites/index.js
@@ -1,6 +1,7 @@
 /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
 
-import React, { useRef, useEffect } from 'react';
+// import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './style.css';
@@ -8,12 +9,12 @@ import { List, ListItem } from '../List';
 import DeleteBtn from '../DeleteBtn';
 
 export default function UserFavorites({ deleteFavorite, favorites }) {
-  const messagesEndRef = useRef(null);
-  const scrollToBottom = () => {
-    if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({behavior: "smooth"});
-    }
-  }
+  // const messagesEndRef = useRef(null);
+  // const scrollToBottom = () => {
+  //   if (messagesEndRef.current) {
+  //     messagesEndRef.current.scrollIntoView({behavior: "smooth"});
+  //   }
+  // }
   /* This works well but needs a fixed height layout to avoid jarring the user */
   // useEffect(scrollToBottom, [favorites]);
 
@@ -36,7 +37,7 @@ export default function UserFavorites({ deleteFavorite, favorites }) {
               && <img width="300px" src={plant.images[0].url} alt={plant.scientific_name} />}
           </ListItem>
         ))}
-        <div ref={messagesEndRef} />
+        {/* <div ref={messagesEndRef} /> */}
       </List>
     );
   }
