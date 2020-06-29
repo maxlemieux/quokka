cb0d7333ece6a29ed194e9894f280b855bd2aba8
Max Lemieux <max.lemieux@gmail.com>
2020-06-28 19:05:11 -0700

    geoip endpoint

--- routes/api/geoip.js
+++ routes/api/geoip.js
@@ -13,13 +13,11 @@ const maxmind = require('maxmind');
 router.route('/:ip')
   .get((req, res) => {
     const { ip } = req.params;
-    maxmind.open('../../geoip/GeoLite2-City.mmdb')
+    console.log(ip);
+    maxmind.open('./geoip/GeoLite2-City.mmdb')
       .then((lookup) => {
-        res.json(lookup.get(ip).postal.code);
+        res.json(lookup.get(ip));
       })
-      .catch((err) => {
-        res.status(err.response.status).send(err.response.statusText);
-      });
   });
 
 module.exports = router;
