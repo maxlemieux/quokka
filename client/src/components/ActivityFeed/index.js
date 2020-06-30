776ba98d27a1a9f87db15c82fc0a411318ff2c25
Andy <ajiajiechen@gmail.com>
2020-06-29 22:38:48 -0700

    adjusted image size

--- client/src/components/ActivityFeed/index.js
+++ client/src/components/ActivityFeed/index.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
+import { Col, Row, Container } from '../Grid';
 
 export default function ActivityFeed(props) {
   return (
@@ -10,9 +11,18 @@ export default function ActivityFeed(props) {
       <ul>
         {props.data.map((favorite) => (
           <li className='list-overflow-container' key={favorite._id}>
-            {favorite.images[0] && <img width='100%' src={favorite.images[0].url} alt={favorite.scientific_name} />}
-            {favorite.user_name} quokked {favorite.common_name
-              ? favorite.common_name : favorite.scientific_name}
+                <Container fluid>
+                  <Row>
+                  {/* What should I plant column */}
+                    <Col size="md-6">
+                      {favorite.images[0] && <img width='50%' src={favorite.images[0].url} alt={favorite.scientific_name} />}
+                    </Col>
+                    <Col size="md-6">
+                    {favorite.user_name} quokked {favorite.common_name
+                      ? favorite.common_name : favorite.scientific_name}
+                    </Col>
+                  </Row>
+                </Container>
           </li>
         ))}
       </ul>
