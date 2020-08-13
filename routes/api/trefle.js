c0844eab9e422c38fad7fc37000f54339a91d100
Max Lemieux <max.lemieux@gmail.com>
2020-08-13 11:57:43 -0700

    moved to v1.6 trefle API

--- routes/api/trefle.js
+++ routes/api/trefle.js
@@ -15,9 +15,11 @@ router.route('/species/:plantId')
 
 router.route('/temperature_minimum_deg_f/:minTemp')
   .get((req, res) => {
-    axios.get(`https://v0.trefle.io/api/species?token=${process.env.REACT_APP_TREFLE}&temperature_minimum_deg_f>${req.params.minTemp}`)
+    // axios.get(`https://v0.trefle.io/api/species?token=${process.env.REACT_APP_TREFLE}&temperature_minimum_deg_f>${req.params.minTemp}`)
+    // https://trefle.io/api/v1/species?token=cHhTeGd4R21rUFc3b2tMK0x4bDNqQT09&range[minimum_temperature_deg_f]=,60
+    axios.get(`https://trefle.io/api/v1/species?token=${process.env.REACT_APP_TREFLE}&range[minimum_temperature_deg_f]=,${req.params.minTemp}`)
       .then((response) => {
-        res.json(response.data);
+        res.json(response.data.data);
       })
       .catch((err) => {
         console.log('backend axios error getting trefle data on min temp');
@@ -27,9 +29,10 @@ router.route('/temperature_minimum_deg_f/:minTemp')
 
 router.route('/name/:name')
   .get((req, res) => {
-    axios.get(`https://v0.trefle.io/api/species?token=${process.env.REACT_APP_TREFLE}&q=${req.params.name}`)
+    // axios.get(`https://v0.trefle.io/api/species?token=${process.env.REACT_APP_TREFLE}&q=${req.params.name}`)
+    axios.get(`https://trefle.io/api/v1/plants/search?q=${req.params.name}&token=${process.env.REACT_APP_TREFLE}`)
       .then((response) => {
-        res.json(response.data);
+        res.json(response.data.data);
       })
       .catch((err) => {
         console.log('backend axios error getting trefle data on name');
