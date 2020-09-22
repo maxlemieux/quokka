fee5aafbe250e2c8e3dbb372369928a235a10cc8
Andy <ajiajiechen@gmail.com>
2020-09-22 12:27:16 -0700

    added more detail to schema of plants

---
+++ client/src/components/Detail/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Detail = () => {
+    const image = { url: 'https://via.placeholder.com/300/d3d3d3/000000?text=No%20image%20available' };
+    return (
+        <div></div>
+    )
+}
+
+export default Detail;
--- models/plant.js
+++ models/plant.js
@@ -1,14 +1,36 @@
 const mongoose = require('mongoose');
+const { string } = require('prop-types');
 
 const { Schema } = mongoose;
 
 const plantSchema = new Schema({
   trefle_id: String,
   common_name: String,
+  slug : String,
   scientific_name: { type: String, required: true },
-  soils_adaptation: Object,
+  main_species_id: String,
+  image_url: String,
+  year: String,
+  bibliography: String,
+  author: String,
+  family_common_name: String,
+  genus_id: String,
+  observation: String,
+  vegetable: Boolean,
+  links: Object,
+  main_species: Object,
+  genus: Object,
+  family: Object, 
+  species: Object, 
+  subspecies: Array, 
+  varieties: Array, 
+  hybrids: Array, 
+  forms: Array, 
+  subvarieties: Array, 
+  sources: Array, 
+  // soils_adaptation: Object,
   images: Array,
-  growth: Object,
+  // growth: Object,
   user_name: {
     type: String,
   },
