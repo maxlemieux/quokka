e8dad7c4aff833a332f8541d584b247fc2a9dbbc
Max Lemieux <max.lemieux@gmail.com>
2020-10-12 10:16:00 -0700

    fixed warnings in console

--- client/src/components/UserFavorites/index.js
+++ client/src/components/UserFavorites/index.js
@@ -8,8 +8,7 @@ import { List, ListItem } from '../List';
 import DeleteBtn from '../DeleteBtn';
 
 export default function UserFavorites({ deleteFavorite, favorites }) {
-  const image = { url: 'https://via.placeholder.com/300/d3d3d3/000000?text=No%20image%20available' };
-  // console.log(favorites);
+  // const image = { url: 'https://via.placeholder.com/300/d3d3d3/000000?text=No%20image%20available' };
 
   if (favorites.length) {
     return (
@@ -21,7 +20,7 @@ export default function UserFavorites({ deleteFavorite, favorites }) {
             <p>({favorite.plantInfo[0].scientific_name})</p>
             <p>{favorite.plantInfo[0].common_name} is in the {favorite.plantInfo[0].family_common_name}</p>
             <p><small>Trefle plant ID: {favorite.trefle_id}</small></p>            
-            <p><img width="150" height="150" src={favorite.plantInfo[0].images} /></p>
+            <p><img width="150" height="150" alt="plant" src={favorite.plantInfo[0].images} /></p>
             <p>Author: {favorite.plantInfo[0].author}</p>
             <br />
           </ListItem>
