faadd9a065b122d15be273f93dfe15ee0ac68b45
Max Lemieux <max.lemieux@gmail.com>
2020-10-12 10:35:22 -0700

    destructured props for ActivityFeed

--- client/src/components/ActivityFeed/index.js
+++ client/src/components/ActivityFeed/index.js
@@ -6,27 +6,27 @@ import './style.css';
 import { Col, Row, Container } from '../Grid';
 import { List, ListItem } from '../List';
 
-export default function ActivityFeed(props) {
+export default function ActivityFeed({ activityData }) {
   // const image = { url: 'https://via.placeholder.com/300/d3d3d3/000000?text=No%20image%20available' };
   // if the data is not in the database
   // console.log(props.activityData)
-  if (props.activityData.length > 0) {    
+  if (activityData.length > 0) {    
     return (
       <List>
-          {props.activityData.map((favorite) => (
+          {activityData.map((favorite) => (
             <ListItem className='list-overflow-container' key={favorite._id}>
-                  <Container fluid>
-                    <Row>
-                      <Col size="md-6" >
-                      {/* <img width='100%' src={favorite.plantInfo[0].images} alt={favorite.scientific_name} /> */}
-                      <img width='100%' src="https://via.placeholder.com/300/d3d3d3/000000?text=No%20image%20available" alt={favorite.scientific_name} />
-                      </Col>
-                      <Col size="md-6">
-                      {favorite.user_name} ({favorite.user_zip}) quokked {favorite.common_name
-                        ? favorite.common_name : favorite.scientific_name}
-                      </Col>
-                    </Row>
-                  </Container>
+              <Container fluid>
+                <Row>
+                  <Col size="md-6" >
+                  {/* <img width='100%' src={favorite.plantInfo[0].images} alt={favorite.scientific_name} /> */}
+                  <img width='100%' src="https://via.placeholder.com/300/d3d3d3/000000?text=No%20image%20available" alt={favorite.scientific_name} />
+                  </Col>
+                  <Col size="md-6">
+                  {favorite.user_name} ({favorite.user_zip}) quokked {favorite.common_name
+                    ? favorite.common_name : favorite.scientific_name}
+                  </Col>
+                </Row>
+              </Container>
             </ListItem>
           ))}
       </List>
